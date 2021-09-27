9f74492d9daeba57b747afc842ba8e369ec64b21
Vigleik <visk010@student.kristiania.no>
2021-09-27 14:34:53 +0200

    Test shouldReturnStatusCode (red)

--- src/test/java/HttpClientTest.java
+++ src/test/java/HttpClientTest.java
@@ -14,5 +14,8 @@ public class HttpClientTest {
         assertEquals(200,
                 new HttpClient("httpbin.org", 80, "/html")
                         .getStatusCode());
+        assertEquals(404,
+                new HttpClient("httpbin.org", 80, "/no-such-page")
+                        .getStatusCode());
     }
 }
