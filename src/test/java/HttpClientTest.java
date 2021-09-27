5d9783f3f0eacfdd545867a38ede462dba295e4a
Vigleik <visk010@student.kristiania.no>
2021-09-27 14:04:01 +0200

    Initial Project

---
+++ src/test/java/HttpClientTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HttpClientTest {
+    @Test
+    void shouldDoSomething() {
+        assertEquals(200, 100+200);
+    }
+}
